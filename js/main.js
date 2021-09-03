0fa69a381e4d845097ffd023a73bce8961030b99
Nguyễn Tuấn <67812579+tuancutedeptrai@users.noreply.github.com>
2021-09-03 16:12:01 +0700

    Up music test for bug

--- js/main.js
+++ js/main.js
@@ -57,38 +57,55 @@ const arrJPWord = new Map();
 .set('n','ん'); 
 var bongno  = new Audio('./music/Tieng-bong-bong-no-www_tiengdong_com.mp3');
 
+var music = document.querySelector('#musicnen');
+
 var arrJPWordValue = Array.from(arrJPWord.values());
 var arrTransWord = Array.from(arrJPWord.keys());
-console.log(arrTransWord);
+var load_music_success = false;
+
 const chieurong = window.innerWidth - 100;
 const chieucao = window.innerHeight;
 var end = false;
 var rdCreateBall ;
-console.log("rong"+chieurong+"Cao"+chieucao);
- function createbuble(color,timefall){
+// code logic refesh and level up ... 
+var time_Ball_Fall = 5;
+var time_LoopInteval = 1000;
+
+
+
+
+// 
+ function createbuble(color,timefall,type){
      var toadoX = Math.random() * (chieurong - 0) + 0;
      var ramdom = Math.floor(Math.random() * (arrJPWord.size - 1 + 1) + 0);  
-    $('.cover-all').append(`<span  value='${arrTransWord[ramdom]}' class="buble" style="left:${toadoX}px;background:${color}" >${arrJPWordValue[ramdom]}</span>`);
-
+     if(type == 'yes')
+     {
+        $('.cover-all').append(`<span diem='${type}'  value='${arrTransWord[ramdom]}' class="buble sao" style="left:${toadoX}px;background:${color}; animation-duration: ${timefall}s;" >${arrJPWordValue[ramdom]}</span>`);
+     }
+     else if(type ==  'no'){
+       $('.cover-all').append(`<span diem='${type}'  value='${arrTransWord[ramdom]}' class="buble" style="left:${toadoX}px;background:${color};animation-duration: ${timefall}s" >${arrJPWordValue[ramdom]}</span>`);
+     }
+     else if(type == 'none')
+     {
+         $('.cover-all').append(`<span diem='${type}'  value='${arrTransWord[ramdom]}' class="buble" style="left:${toadoX}px;background:${color};animation-duration: ${timefall}s" >${arrJPWordValue[ramdom]}</span>`);
+     }
 }
 var divcha = document.querySelector('.cover-all');
 divcha.addEventListener('animationend',(e)=>{
  //  clearInterval(interval);
 })
-
-
 var interval = setInterval(()=>{
  rdCreateBall  = Math.random(0,1);
- if(!document.querySelector('#pause').checked)
- {
-    createbuble('blue');
- }
-
-    // if(rdCreateBall<0.9 && !document.querySelector('#pause').checked ){
-    // }
-    // else if(rdCreateBall>0.8 && rdCreateBall<0.9){        
-    // }
-},1000);
+    if(rdCreateBall<0.8 && !document.querySelector('#pause').checked ){
+        createbuble('blue','7','none');
+    }
+    else if(rdCreateBall>=0.8 && rdCreateBall<0.9 && !document.querySelector('#pause').checked){
+        createbuble('blue','7','yes');
+    }
+    else if(rdCreateBall>=0.9 && !document.querySelector('#pause').checked){
+        createbuble('black','7','no');
+    }
+},time_LoopInteval);
 
 
 
@@ -97,9 +114,17 @@ document.querySelector('#input-text').addEventListener('keypress',(e)=>{
    let   allbong = document.querySelectorAll('.buble');
       allbong.forEach(element => {
             if(e.target.value == element.getAttribute('value')){
+               if(element.getAttribute('diem')=='none'){
                 element.remove();
                 pointup();
-               
+               }
+               else if(element.getAttribute('diem')=='yes')
+               {
+                element.remove();
+                pointup();
+                pointup();
+               }
+              
             }
       });
       e.target.value = "";
@@ -112,6 +137,10 @@ window.addEventListener('click',(e)=>{
     if(cb.checked && e.target.getAttribute('class') == 'buble'){
        e.target.remove();
        pointup();
+    }
+    if(e.target.getAttribute('diem')=='no')
+    {
+        e.target.remove();
     }
 })
 
@@ -131,13 +160,42 @@ function changeStatus(status_name,visible){
 }
 
 document.querySelector('#pause').addEventListener('change',(e)=>{
-  
     if(e.target.checked){
         changeStatus('paused','hidden');
         document.querySelector('#input-text').style.visibility = 'hidden';
+        // if(load_music_success){
+             music.pause();
+        // }
     }
     else{
         changeStatus('running','visible');
-        document.querySelector('#input-text').style.visibility = 'visible';       
+        document.querySelector('#input-text').style.visibility = 'visible'; 
+    //    if(load_music_success){
+          music.play();
+    //     }  
     }
 })
+
+document.querySelector('.btn-resart').onclick = function(){
+    location.reload();
+}
+
+
+document.querySelector('#bgmusic').addEventListener('change',(e)=>{
+    if(e.target.checked){
+        music.muted = false;
+       
+        document.querySelector('#icon_loa').classList = ' fas fa-volume-up'
+    }
+    else{
+        music.muted = true;
+        document.querySelector('#icon_loa').classList = 'fas fa-volume-mute'
+    }
+})
+
+
+
+
+
+
+
